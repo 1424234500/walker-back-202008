fb4f89b9aac6074305cc8dbe65ed83ed56ae7ca3
Walker <1424234500@qq.com>
2019-04-03 19:57:29 +0800

    maven project create of basessm

---
+++ walker-core/src/main/java/com/walker/common/util/XmlUtil.java
@@ -0,0 +1,15 @@
+package com.walker.common.util;
+
+
+/**
+ * xml工具类
+ */
+public class XmlUtil {
+
+	public static String toFullXml(Bean bean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+    
+}
