239ef9250748e0b4b70d7c7680550eb2d810ef55
Walker <1424234500@qq.com>
2019-06-28 17:37:10 +0800

    2019年 06月 28日 星期五 17:37:09 CST

--- walker-core/src/test/java/com/walker/core/database/RedisTest.java
+++ walker-core/src/test/java/com/walker/core/database/RedisTest.java
@@ -2,7 +2,13 @@ package com.walker.core.database;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import com.walker.common.util.Tools;
+import com.walker.core.database.Redis.Fun;
+import com.walker.core.mode.Watch;
 
 import redis.clients.jedis.*;
 
@@ -16,5 +22,43 @@ public class RedisTest {
 		j.set("aaa", "bbb");
 		assertEquals(j.get("aaa"), "bbb");
 	}
+	@Test
+	public void testPipeline() {
+		
+		Redis.doJedis(new Fun<Object>() {
+
+			@Override
+			public Object make(Jedis jedis) {
+				Watch w = new Watch("test pipe ");
+				int count = 200;
+				for(int i = 0; i < count; i++) {
+					jedis.set("test:pipe:" + i, "value" + i);
+				}
+				w.cost("set " + count);
+				Pipeline pipe = jedis.pipelined();
+				int pc = 200;
+				for(int i = 0; i < pc; i++) {
+					pipe.set("test:pipe:set:"+i, "pc:"+i);
+				}
+				w.cost("pipeSet " + pc);
+				for(int i = 0; i < count; i++) {
+					pipe.get("test:pipe:" + i);
+				}
+				w.cost("pipeGet " + count);
+				pipe.del("test:pipe*");
+
+				List<Object> res = pipe.syncAndReturnAll();
+				Tools.formatOut(res);
+				Tools.out(w.res());
+				
+				return null;
+			}
+		});
+		
+		
+	}
+	
+	
+	
 
 }
