c117b9a6f93a9eda6fb6b704a4257e76b29f5451
Walker <1424234500@qq.com>
2019-09-04 20:00:08 +0800

    2019年 09月 04日 星期三 20:00:08 CST

--- walker-service/src/main/java/com/walker/service/Launcher.java
+++ walker-service/src/main/java/com/walker/service/Launcher.java
@@ -1,5 +1,6 @@
 package com.walker.service;
 
+import com.walker.common.util.ThreadUtil;
 import com.walker.common.util.Tools;
 import com.walker.dubbo.DubboMgr;
 
@@ -10,23 +11,23 @@ import java.util.concurrent.TimeUnit;
 
 public class Launcher {
     Launcher(){
-        DubboMgr.getInstance().setDubboXml("dubbo-service-config.xml");
-
+        DubboMgr.getInstance().setDubboXml("dubbo-service-config.xml").start();
 
         MessageService service  = DubboMgr.getService("messageService");
         Tools.out("dubbo", service.sizeMsg());
-
-        ScheduledExecutorService sch = Executors.newSingleThreadScheduledExecutor();
-        sch.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                Tools.out("--------------");
-                MessageService service  = DubboMgr.getService("messageService");
-
-                Tools.out("dubbo", "messageService", service.sizeMsg());
-
-            }
-        }, 1000, 10000, TimeUnit.MILLISECONDS);
+//
+//        ScheduledExecutorService sch = Executors.newSingleThreadScheduledExecutor();
+//        sch.scheduleAtFixedRate(new Runnable() {
+//            @Override
+//            public void run() {
+//                Tools.out("--------------");
+//                MessageService service  = DubboMgr.getService("messageService");
+//
+//                Tools.out("dubbo", "messageService", service.sizeMsg());
+//
+//            }
+//        }, 1000, 10000, TimeUnit.MILLISECONDS);
+        ThreadUtil.sleep(22222222222L);
     }
 
     public static void main(String[] argv){
