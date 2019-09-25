0ecea531bd8005017818e04147223c46b3de49e4
Walker <1424234500@qq.com>
2019-09-25 09:07:27 +0800

    2019年 09月 25日 星期三 09:07:27 CST

--- walker-web/vue/src/utils/http.js
+++ walker-web/vue/src/utils/http.js
@@ -27,38 +27,62 @@ axios.interceptors.request.use(function (config) {
   return Promise.reject(error);
 });
 
-axios.interceptors.response.use(function (response){
+axios.interceptors.response.use(function (response) {
   // 处理响应数据
   if (response.status === 200) {
     return Promise.resolve(response);
   } else {
     return Promise.reject(response);
   }
-}, function (error){
+}, function (error) {
   // 处理响应失败
   return Promise.reject(error);
 });
 
-export function get(url, params){
-  return new Promise((resolve, reject) =>{
+
+
+
+export function get(url, params) {
+  before(url, params, 'get')
+  return new Promise((resolve, reject) => {
     axios.get(url, {
       params: params
     }).then(res => {
+      afterResolve(url, params, 'get', res)
       resolve(res.data);
-}).catch(err =>{
-    reject(err.data)
-})
-});
+    }).catch(err => {
+      afterReject(url, params, 'get', err)
+      reject(err.data)
+    })
+  });
 }
 
 export function post(url, params) {
   return new Promise((resolve, reject) => {
+    before(url, params, 'post')
     axios.post(url, qs.stringify(params))
       .then(res => {
+      afterResolve(url, params, 'post', res)
       resolve(res.data);
-})
-.catch(err =>{
-    reject(err.data)
-})
-});
+    }).catch(err => {
+      afterReject(url, params, 'post', err)
+      reject(err.data)
+    })
+}
+
+var pre = "http.js "
+function before(url, params, type){
+  console.info(pre + " before " + type + " " + url + " " + JSON.stringify(params) )
+
+}
+function afterResolve(url, params, type, res){
+  console.info(pre + " afterResolve " + type + " " + url + " " + JSON.stringify(params) )
+  console.info(res)
+
+}
+function afterReject(url, params, type, err){
+  console.info(pre + " afterReject " + type + " " + url + " " + JSON.stringify(params) )
+  console.info(err)
+
+
 }
