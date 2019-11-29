2194d0f9153d78a87bab0449837e32d470166ba9
Walker <1424234500@qq.com>
2019-11-29 20:00:16 +0800

    2019年 11月 29日 星期五 20:00:16 CST

--- walker-web/vue/src/views/dashboard/index.vue
+++ walker-web/vue/src/views/dashboard/index.vue
@@ -1,61 +1,31 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ user['NAME'] }}</div>
+    <component :is="currentRole" />
   </div>
 </template>
 
 <script>
-import { getToken,setToken,getUser,setUser } from '@/utils/cookie' // get token from cookie
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+import editorDashboard from './editor'
 
 export default {
-    created() {
-      this.user = getUser()
-
-
-    },
-    filters: {
-      statusFilter(status) {
-        const statusMap = {
-          published: 'success',
-          draft: 'gray',
-          deleted: 'danger'
-        }
-        return statusMap[status]
-      }
-    },
-    data() {
-      return {
-        user: null,
-
-      }
-    },
-    methods: {
-      //查询展示的行列信息 备注
-      getColumns() {
-        this.loadingCols = true
-        this.get('/common/getColsMap.do', {tableName: 'W_ROLE_USER'}).then((res) => {
-          this.colMap = res.data.colMap
-          this.colKey = res.data.colKey
-          this.clearRowSearch()
-          this.loadingCols = false
-          this.getListPage()
-        }).catch(() => {
-          this.loadingCols = false
-        })
-
-      },
+  name: 'Dashboard',
+  components: { adminDashboard, editorDashboard },
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
     }
   }
-</script>
-
-<style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
 }
-</style>
+</script>
