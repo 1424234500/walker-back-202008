8abd79b8f005a3aaa37ad6278b3c540d8923bb48
Walker <1424234500@qq.com>
2020-02-25 18:43:25 +0800

    2020年 02月 25日 星期二 18:43:24 CST

--- walker-web/vue/src/layout/components/Navbar.vue
+++ walker-web/vue/src/layout/components/Navbar.vue
@@ -7,7 +7,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="userSocket-avatar">
+          <img :src="'https://avatars2.githubusercontent.com/u/7111694?s=460&v=4'" class="userSocket-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="userSocket-dropdown">
